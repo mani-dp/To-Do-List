603bd4290e17c20b1620347a773c06c4633909a3
mani-Darvish <mani.darvish2020@gmail.com>
2026-08-16 22:21:04 +0330

    create api layer

---
+++ src/core/api/projectsApi/getproject.ts
@@ -0,0 +1,23 @@
+const Getproject = async () => {
+    try {
+        const response = await fetch("https://67c8a9fb0acf98d070875d87.mockapi.io/Todo", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+
+        if (!response.ok) {
+            throw new Error(`HPPT error status : ${response.status}`)
+        }
+
+        const data = response.json();
+        return data
+    } catch (err) {
+        console.log("error fetch data", err);
+        throw err
+    }
+
+}
+
+export default Getproject
