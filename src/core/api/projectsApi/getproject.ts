aa28ae16b60601af894cc9b1f33ca74d7cb4e88f
mani-Darvish <mani.darvish2020@gmail.com>
2026-08-17 23:59:46 +0330

    test

--- src/core/api/projectsApi/getproject.ts
+++ src/core/api/projectsApi/getproject.ts
@@ -1,34 +1,25 @@
-// const Getproject = async () => {
-//     try {
-//         const response = await fetch("https://67c8a9fb0acf98d070875d87.mockapi.io/Todo", {
-//             method: "GET",
-//             headers: {
-//                 "Content-Type": "application/json"
-//             }
-//         })
+const Getproject = async () => {
+    try {
+        const response = await fetch("https://67c8a9fb0acf98d070875d87.mockapi.io/Todo", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
 
-//         if (!response.ok) {
-//             throw new Error(`HPPT error status : ${response.status}`)
-//         }
-
-//         const data = response.json();
-//         return data
-//     } catch (err) {
-//         console.log("error fetch data", err);
-//         throw err
-//     }
-
-// }
-
-// export default Getproject
-
-const GetProject = () => {
-    const response = await fetch("https://67c8a9fb0acf98d070875d87.mockapi.io/Todo", {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json"
+        if (!response.ok) {
+            throw new Error(`HPPT error status : ${response.status}`)
         }
-    })
+
+        const data = response.json();
+        return data
+    } catch (err) {
+        console.log("error fetch data", err);
+        throw err
+    }
+
 }
 
-export default GetProject
+
+
+export default Getproject
